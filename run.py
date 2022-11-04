81cd0f244c9f478d804ca98b3ea700f6e607f292
bartick <bartickmaiti1998@gmail.com>
2022-11-04 13:28:41 +0530

    show image

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from src import screen
 
 product_screen = screen.Screen(800, 600)
-product_screen.draw(100, 100, "red")
+product_screen.showImage('unknown.png')
 product_screen.mainloop()
--- src/screen.py
+++ src/screen.py
@@ -11,6 +11,11 @@ class Screen:
     def draw(self, x, y, color):
         self.canvas.create_rectangle(x, y, x+100, y+100, fill=color)
         self.canvas.update()
+    
+    def showImage(self, url):
+        self.image = tkinter.PhotoImage(file=url)
+        self.canvas.create_image(0, 0, anchor=tkinter.NW, image=self.image)
+        self.canvas.update()
 
     def mainloop(self):
         self.root.mainloop()
